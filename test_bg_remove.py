1a17898c30a4d466ec0b9474ffada8746caea3cf
EkaterinaSeveryanka <117996969+EkaterinaSeveryanka@users.noreply.github.com>
2023-06-15 23:05:05 +0500

    Update test_bg_remove.py

--- test_bg_remove.py
+++ test_bg_remove.py
@@ -5,6 +5,30 @@ from io import BytesIO
 import re
 
 
+def is_russian(word):
+    """Проверяем, что в строке только русские буквы"""
+    return bool(re.match("^[А-Яа-яЁё ]+$", word))
+
+
+def convert_image(img):
+    buf = BytesIO()
+    img.save(buf, format="PNG")
+    byte_im = buf.getvalue()
+    return byte_im
+
+
+def fix_image(upload):
+    image = Image.open(upload)
+    col1.write("Original Image :camera:")
+    col1.image(image)
+
+    fixed = remove(image)
+    col2.write("Fixed Image :wrench:")
+    col2.image(fixed)
+    st.sidebar.markdown("\n")
+    st.sidebar.download_button("Download fixed image", convert_image(fixed), "fixed.png", "image/png")
+
+
 def test_is_russian():
     # Проверяем, что регулярное выражение работает корректно
     assert is_russian("") == True
